4c2c65dfa8e0e429f78baff5d28af2fbe251ccd8
Diogo Santos <diogo.s.pedroso@gmail.com>
2023-04-01 17:13:02 -0300

    Organizando e recebendo as preps para o BD

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app_videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Diogo"
+          description="Brecker o goleiro"
+          music="musica épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+      <Video 
+          likes={444}
+          messages={555}
+          shares={666}
+          name="Paulo Santos"
+          description="Bird olhando para a camera"
+          music="Clap your hands"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
